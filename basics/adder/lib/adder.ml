7d86f167070f9dcdf63dff67fea852810e7dee31
Docker <docker@example.com>
2024-12-11 08:33:04 +0000

    adder

--- basics/adder/lib/adder.ml
+++ basics/adder/lib/adder.ml
@@ -1,4 +1,6 @@
 (* val addlist : int list -> int *)
 (* addlist l adds the element of the list of integers l *)
 
-let addlist l = 0 (* replace 0 with actual code *)
+let rec addlist l = match l with
+| [] -> 0 (* Base case: the sum of an empty list is 0 *)
+| x :: xs -> x + addlist xs (* Sum the head with the result of the recursive call on the tail *)
